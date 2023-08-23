e87553d5c2f6685a4ff8d595a26ba11880f369e4
Dmitry Murashev <murashev@gmail.com>
2023-08-23 15:07:30 +0400

    add corrected Lion with 1 constructor

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 public class Lion {
 
-    private boolean hasMane;
-    private String sex;
-    private Feline feline;
 
-    public Lion(String sex) throws Exception {
+    private Feline feline;
+    boolean hasMane;
+
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -16,13 +17,6 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
-    }
-    // Алеся, привет!  Я убрал создание объекта и добавил конструктор, как ты указала.
-    // Правда, на мой неофитский взгляд, передавать в него параметр sex - лишнее. Тесты сразу его затребовали (их тоже изменил),
-    // а тестовой нагрузки он, опять же на мой взгляд, не несет.
-    public Lion(String sex, Feline feline) {
-        this.feline = feline;
-        this.sex = sex;
     }
 
     public int getKittens() {
